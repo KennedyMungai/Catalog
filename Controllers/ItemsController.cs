a2aa602444d94e7d686dab4c839b8c675ae18619
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-20 20:59:35 +0300

    Introduced and assigned the repository class

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,9 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 
+using Catalog.Repositories;
+
 namespace Catalog.Controllers;
 
+//GET /Items
 [ApiController]
+[Route("[controller]")]
 public class ItemsController : ControllerBase
 {
-    
+    private readonly InMemItemsRepository? repository;
+
+    public ItemsController()
+    {
+        repository = new InMemItemsRepository();
+    }
 }

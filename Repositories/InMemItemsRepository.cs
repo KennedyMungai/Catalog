3b05db45235d06b93c52584f11b5ffc283a868d9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-21 14:10:15 +0300

    Implemented the interface

--- Repositories/InMemItemsRepository.cs
+++ Repositories/InMemItemsRepository.cs
@@ -2,7 +2,8 @@ using Catalog.Entities;
 
 namespace Catalog.Repositories;
 
-public class InMemItemsRepository
+
+public class InMemItemsRepository : IInMemItemsRepository
 {
     private readonly List<Item> items = new()
     {

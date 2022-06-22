8d392c9b2af83038e79c88b2efa2ab529b63dadb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-22 12:19:54 +0300

    Corrected a mistake

--- Repositories/IItemsRepository.cs
+++ Repositories/IItemsRepository.cs
@@ -4,7 +4,7 @@ namespace Catalog.Repositories;
 
 public interface IItemsRepository
 {
-    Task GetItemAsync(Guid id);
+    Task<Item> GetItemAsync(Guid id);
     Task<IEnumerable<Item>> GetItemsAsync();
     Task CreateItemAsync(Item item);
     Task UpdateItemAsync(Item item);

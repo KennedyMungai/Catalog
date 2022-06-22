ec974956a05763622668468dab77b8d300af4525
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-22 10:40:32 +0300

    Added BsonSerializers for Guid and DateTime Offset

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using System.Data.Common;
 using Catalog.Repositories;
 using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 builder.Services.Configure<MongoDbItemsRepository>(builder.Configuration.GetSection("MongoDbSettings"));
 builder.Services.AddSingleton<IInMemItemsRepository, MongoDbItemsRepository>();
 

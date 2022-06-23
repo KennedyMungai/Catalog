a8f07750f3ab169b131f1709a924043be46f2456
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-23 16:12:16 +0300

    Created a test for the GetItemAsync method

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -1,10 +1,30 @@
+using Catalog.Api.Controllers;
+using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+
 namespace Catalog.UnitTests;
 
 public class ItemsControllerTests
 {
     [Fact]
-    public void Test1()
+    public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
     {
+        // Arrange
+        var repositoryStub = new Mock<IItemsRepository>();
+        repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+                        .ReturnsAsync((Item)null);
 
+        var loggerStub = new Mock<ILogger<ItemsController>>();
+
+        var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+        // Act
+        var result = await controller.GetItemAsync(Guid.NewGuid());
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
     }
 }

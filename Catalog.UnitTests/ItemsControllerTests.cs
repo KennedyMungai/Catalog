297e9edc7e8f909bb45678b22352ef747a8efd20
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-23 16:33:51 +0300

    Finished writing the GetItem with value test

--- Catalog.UnitTests/ItemsControllerTests.cs
+++ Catalog.UnitTests/ItemsControllerTests.cs
@@ -1,4 +1,5 @@
 using Catalog.Api.Controllers;
+using Catalog.Api.Dtos;
 using Catalog.Api.Entities;
 using Catalog.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -33,10 +34,22 @@ public class ItemsControllerTests
     public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
     {
         // Given
-        
+        var expectedItem = CreateRandomItem();
+
+        repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+            .ReturnsAsync(expectedItem);
+
+        var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
         // When
-    
+        var result = await controller.GetItemAsync(Guid.NewGuid());
+
         // Then
+        Assert.IsType<ItemDto>(result.Value);
+        var dto = (result as ActionResult<ItemDto>).Value;
+        Assert.Equal(expectedItem.Id, dto.Id);
+        Assert.Equal(expectedItem.Name, dto.Name);
+        Assert.Equal(expectedItem.Price, dto.Price);
+        Assert.Equal(expectedItem.CreatedDate, dto.CreatedDate);
     }
 
     private Item CreateRandomItem()

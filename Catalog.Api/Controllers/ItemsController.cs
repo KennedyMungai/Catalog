012bd66c43c6f1ee6cb203cae0ea40264b57e2a6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-06-23 18:17:48 +0300

    Updated the UpdateItemAsync method

--- Catalog.Api/Controllers/ItemsController.cs
+++ Catalog.Api/Controllers/ItemsController.cs
@@ -72,12 +72,10 @@ public class ItemsController : ControllerBase
             return NotFound();
         }
 
-        Item updatedItem = existingItem with {
-            Name = itemDto.Name, 
-            Price = itemDto.Price
-        };
+        existingItem.Name = itemDto.Name;
+        existingItem.Price = itemDto.Price;
 
-        await repository.UpdateItemAsync(updatedItem);
+        await repository.UpdateItemAsync(existingItem);
 
         return NoContent();
     }
